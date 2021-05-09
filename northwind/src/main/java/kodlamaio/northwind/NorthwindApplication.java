fa5b6a46f9601ebf8f272e1510f5af0cbef44a29
sergennkeles <65495192+sergennkeles@users.noreply.github.com>
2021-05-09 14:51:54 +0300

    swagger added

--- northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -3,7 +3,10 @@ package kodlamaio.northwind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
